3d8b38c24788b7e3df699102e502b9594540b6ae
EmekaRaphael <emekaraphael13@gmail.com>
2024-01-20 21:05:31 +0100

    lihkn

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { EditNoteSharpIcon } from "@mui/material";
+
+function Header() {
+  return (
+    <header>
+      <h1>{<EditNoteSharpIcon fontSize="medium" />}StickNotes</h1>
+    </header>
+  );
+}
+
+export default Header;
